2a3e39c144087a62f78c2104dce9dc4b2186c8c1
AB8007 <AB8007@student.labranet.fi>
2024-10-03 16:25:12 +0300

    fixed CORS error

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,17 @@ import express from 'express'
 import itemsRouter from './routes/items.js'
 import 'express-async-errors'
 import { runAtInterval } from './jobs/fetchDataJob.js'
+import client from './config/redisConfig.js'
 const app = express()
+import cors from 'cors'
+client.connect(() => console.log('Redis connected'))
 
 try {
   runAtInterval(60)
 } catch (error) {
   console.error('Error fetching data:', error)
 }
-
+app.use(cors())
 app.use(express.json())
 app.use('/api', itemsRouter)
 
